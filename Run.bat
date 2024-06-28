ee05ab74efdb8fc1a16be1bac7ca2e4553e35d1f
Ruturaj0861 <125799539+Ruturaj0861@users.noreply.github.com>
2024-06-28 16:20:02 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,4 +1,5 @@
 pytest -s -v -m "sanity and regression" --html=./Report/report.html R:\Demoblaze_E-commerce\testCases
+REM pytest -s -v -m "sanity and regression" --html=./Report/report.html R:\Demoblaze_E-commerce\testCases
 REM pytest -s -v -m "sanity" --html=./Report/report.html R:\Demoblaze_E-commerce\testCases --browser chrome
 REM pytest -s -v -m "regression" --html=./Report/report.html R:\Demoblaze_E-commerce\testCases --browser chrome
 REM pytest -s -v -m "sanity and regression" --html=./Report/report.html R:\Demoblaze_E-commerce\testCases --browser firefox
